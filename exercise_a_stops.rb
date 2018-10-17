7e9c7cfbc87cae43e5bd7f810db1f7ae5833416e
Graeme Congreve <junkmailgmc@gmail.com>
2018-10-17 16:51:45 +0100

    completed exercise_a

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -12,9 +12,20 @@ p stops
 # 4. Work out the index position of `"Linlithgow"`
 p stops.index("Linlithgow")
 # 5. Remove `"Livingston"` from the array using its name
-stops.remove("Livingston")
+stops.delete("Livingston")
+p stops
 # 6. Delete `"Cumbernauld"` from the array by index
+stops.delete_at(2)
+p stops
 # 7. How many stops there are in the array?
+p stops.length
 # 8. How many ways can we return `"Falkirk High"` from the array?
+p stops[2]
+
 # 9. Reverse the positions of the stops in the array
+p stops.reverse
 # 10. Print out all the stops using a for loop
+
+for stations in stops
+  p stations
+end
